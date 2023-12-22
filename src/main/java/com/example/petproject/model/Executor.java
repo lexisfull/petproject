8581287b476a1fd8723e9a9a1c940a7c082b34b5
aleksey <alexeypanov29@gmail.com>
2023-12-22 18:18:24 +0300

    add annotation

--- src/main/java/com/example/petproject/model/Executor.java
+++ src/main/java/com/example/petproject/model/Executor.java
@@ -4,12 +4,15 @@ import jakarta.persistence.*;
 
 @Entity
 public class Executor {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "age")
     private int age;
 
--- src/main/java/com/example/petproject/model/Person.java
+++ src/main/java/com/example/petproject/model/Person.java
@@ -5,12 +5,15 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "Person")
 public class Person {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "name")
     private String name;
+
     @Column(name = "age")
     private int age;
 

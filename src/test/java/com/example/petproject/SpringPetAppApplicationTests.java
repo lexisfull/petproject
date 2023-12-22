e866ba40428bbd66a347ba9f84341c2cb3d45687
aleksey <alexeypanov29@gmail.com>
2023-12-22 11:41:31 +0300

    Initial commit

---
+++ src/test/java/com/example/petproject/SpringPetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

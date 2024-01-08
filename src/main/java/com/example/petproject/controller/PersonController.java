3cf5a77a8a3224103511e8a9301a794a0fa97da0
aleksey <alexeypanov29@gmail.com>
2024-01-08 22:48:07 +0300

    add swagger

--- src/main/java/com/example/petproject/controller/PersonController.java
+++ src/main/java/com/example/petproject/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.example.petproject.controller;
 
 import com.example.petproject.model.Person;
 import com.example.petproject.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,27 +14,32 @@ import java.util.List;
  */
 @RequestMapping("/persons")
 @RestController
+@Tag(name = "Пользователи", description = "Методы для работы с пользователями")
 public class PersonController {
     @Autowired
     private PersonService personService;
 
 
     @GetMapping
+    @Operation(summary = "Возвращает список пользователей")
     public List<Person> getPearson(){
         return personService.getPersons();
     }
     @GetMapping("/{id}")
+    @Operation(summary = "Возвращает пользователя по идентификатору")
     public Person getPersonById(@PathVariable Integer id){
         return personService.getPersonById(id);
     }
 
 
     @PostMapping
+    @Operation(summary = "Добавляет пользователя")
     public void addPerson(@RequestBody Person person){
         personService.savePerson(person);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаляет пользователя")
     public void deletePerson(@PathVariable("id") Integer id){
         personService.deleteByPerson(id);
     }
--- src/main/java/com/example/petproject/model/Product.java
+++ src/main/java/com/example/petproject/model/Product.java
@@ -1,15 +1,35 @@
 package com.example.petproject.model;
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
+@Entity
+@Table(name = "Product")
 @Data
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Product {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name = "name", nullable = false)
     String name;
+
+    @Column(name = "protein", nullable = false)
     int protein;
+
+    @Column(name = "fat", nullable = false)
     int fat;
+
+    @Column(name = "carbohydrates", nullable = false)
     int carbohydrates;
+
+    @Column(name = "kcal", nullable = false)
     int kcal;
 }

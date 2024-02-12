fba11398866d122f6241663c46eaa3cc96f8508f
Aleksey <alexeypanov29@gmail.com>
2024-02-12 23:19:10 +0300

    add integer

---
+++ src/test/java/com/example/petproject/service/AnalysisServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.petproject.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnalysisServiceTest {
+
+    @Test
+    void getAllAnalysis() {
+    }
+
+    @Test
+    void getAllAnalysisPerson() {
+    }
+
+    @Test
+    void saveAnalysis() {
+    }
+
+    @Test
+    void deleteAnalysis() {
+    }
+}
